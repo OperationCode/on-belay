b163a0784eaa3252c2b727f24a81afeb260f30cf
Aaron Suarez <aaron.j.suarez1@gmail.com>
2017-10-30 21:18:48 -0500

    Add logger to main.py. Fixes #38

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
+from kivy.logger import Logger
 from kivy.utils import get_color_from_hex
 
 
 class OnBelay(App):
-    pass      
+    pass
 
 if __name__ == '__main__':
     Window.clearcolor = get_color_from_hex('#89A894')
